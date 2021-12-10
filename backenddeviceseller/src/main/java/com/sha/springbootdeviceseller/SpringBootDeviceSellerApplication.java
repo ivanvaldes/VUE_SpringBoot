2132371d747b4432a52d9907b7cba0f0ecf7c57e
ivanvaldes <ivmigliore@gmail.com>
2021-12-10 15:24:28 -0500

    SpringBoot structure

---
+++ backenddeviceseller/src/main/java/com/sha/springbootdeviceseller/SpringBootDeviceSellerApplication.java
@@ -0,0 +1,13 @@
+package com.ivanvaldes.backenddeviceseller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDeviceSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDeviceSellerApplication.class, args);
+	}
+
+}
---
+++ backenddeviceseller/src/main/java/com/sha/springbootdeviceseller/model/User.java
@@ -0,0 +1,8 @@
+package com.sha.springbootdeviceseller.model;
+
+import javax.persistence.Table;
+
+
+@Table(name= "users")
+public class User {
+}
